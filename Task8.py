9b7a0876d8e727e83a60eb2ffa3d60822af5e563
PetrTomsik <57231063+PetrTomsik@users.noreply.github.com>
2024-12-04 19:31:30 +0100

    Task 9 -> Porovnánací operítory v praxi

--- Task8.py
+++ Task8.py
@@ -4,6 +4,11 @@ jmeno = "Albert"
 druhe_jmeno = "Jura"
 cislo = 30.4444444444444
 print(f"Ahoj jsem {jmeno} a {druhe_jmeno} a je mi {cislo:.2f} let")   #f sting pred dám f
+# pokud chces naformatot na dve desetiny cisla  (:.2f)
+
+
+print(f"Promenna cislo >6 {cislo>6}")
+
 
 # Prints today's date with help
 # of datetime library
@@ -16,3 +21,13 @@ print(f"{today:%B %d, %Y}")
 print(f"'GeeksforGeeks'")
 print(f"""Geeks"for"Geeks""")
 print(f'''Geeks'for'Geeks''')
+
+english = 78
+maths = 56
+hindi = 85
+
+print(f"Ram got total marks {english + maths + hindi} out of 300")
+
+newline = ord('\n')
+
+print(f"newline: {newline}")
---
+++ Task9.py
@@ -0,0 +1,5 @@
+print(f"True or False {True or False}") # False
+print(f"False or False {False or False}") # False
+print(f"True and False {False and False}") # False
+
+print(f"True and False {True and False or True}") # prvni se udela and a pak or
